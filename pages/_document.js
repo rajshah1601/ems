6d1c2bafdbbd2e128f4a501cf6f2a3dd48272d9d
rajshah1606 <raj.shah@budhanatech.com>
2023-03-23 12:58:43 +0530

    second commit

--- pages/_document.js
+++ pages/_document.js
@@ -4,16 +4,16 @@ export default function Document({ title, description, image, url }) {
   return (
     <Html lang="en">
       <Head>
-        {/* <meta property="og:title" content="Employee Management System" />
+        <meta property="og:title" content="Employee Management System" />
         <meta property="og:description" content="CRUD Application in NEXT JS and MONGO DB" />
-        <meta property="og:image" content="https://photos.app.goo.gl/yWF4xQhrQQfdsf1Z6" /> */}
-        {/* <meta property="og:url" content="Your URL Here" /> */}
+        <meta property="og:image" content="https://photos.app.goo.gl/yWF4xQhrQQfdsf1Z6" /> 
+        {/* <meta property="og:url" content="Your URL Here" />| */}
 
 
-        <meta property="og:title" content={title} />
+        {/* <meta property="og:title" content={title} />
         <meta property="og:description" content={description} />
         <meta property="og:image" content={image} />
-        <meta property="og:url" content={url} />
+        <meta property="og:url" content={url} /> */}
 
 
       </Head>
